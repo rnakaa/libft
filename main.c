168dd37f5e32b962ffe00c6cb9938ee92f56e639
rnakaa <102458195+rnakaa@users.noreply.github.com>
2022-09-14 23:44:40 +0900

    strlcat

---
+++ main.c
@@ -0,0 +1,39 @@
+#include<libc.h>
+#include"libft.h"
+int main(void)
+{
+	
+	// printf("%zu",ft_strlcat(NULL, "src2", 0));
+	printf("%lu", strlcat(NULL, "src2", 0));
+// 	// size_t	size;
+// 	// char	dst[] = "tetete";
+// 	char tmp[100] = {'\0'};
+// 	tmp[0] = '1';
+// 	tmp[1] = '1';
+// 	tmp[2] = '1';
+// 	tmp[3] = '1';
+// 	tmp[10] = '1';
+// 	tmp[11] = '1';
+// 	tmp[12] = '1';
+// 	// char src[100] = {'\0'};
+// 	// src[0]='a';
+// 	// src[1]='a';
+// 	// src[2]='a';
+// 	// src[3]='a';
+// 	// src[10]='a';
+// 	// src[11]='a';
+// 	// src[12]='a';
+// 	char *src;
+
+// 	*src = '\0';
+
+// 	// size = 0;
+// 	// printf("%lu\n", size - ft_strlen(dst) - 1);
+// 	printf("%lu\n", ft_strlcat(tmp, src, 20));
+// 	// printf("%s\n",tmp);
+// 	// printf("10%c\n",tmp[13]);
+// 	// printf("11%c\n",tmp[14]);
+// 	// printf("12%c\n",tmp[15]);
+// 	// printf("%lu", strlcat("12345", "12", 5));
+	return (0);
+}
