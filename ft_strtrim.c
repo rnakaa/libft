008b2cc0bf119204dc3498a0af6f4655dd4efd05
rnakaa <102458195+rnakaa@users.noreply.github.com>
2022-10-08 17:24:05 +0900

    10/08 校舎

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rnaka <rnaka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 16:56:08 by rnaka             #+#    #+#             */
-/*   Updated: 2022/10/08 16:57:04 by rnaka            ###   ########.fr       */
+/*   Updated: 2022/10/08 17:22:42 by rnaka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	char	*str;
+	char	*save;
+	size_t	size;
+
+	size = ft_strlen(s1);
+	save = (char *)s1;
+	while(size != 0 || *save != '\0')
+	{
+		save = ft_strnstr(save, set, ft_strlen(set));
+		if(save != 0)
+		{
+			
+			save += ft_strlen(set) - 1;
+		}
+		save++;
+		size--;
+	}
 }
